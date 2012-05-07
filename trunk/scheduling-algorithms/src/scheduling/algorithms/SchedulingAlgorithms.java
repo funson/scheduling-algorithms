d4df711b0e660450e5e9f1bb0f010d6d240f65a7
miquel.amj@gmail.com <miquel.amj@gmail.com@72b0d667-33a1-be38-4b05-428c798d882c>
2012-05-07 21:37:38 +0000

    Base Project

---
+++ trunk/scheduling-algorithms/src/scheduling/algorithms/SchedulingAlgorithms.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheduling.algorithms;
+
+/**
+ *
+ * @author Miguel
+ */
+public class SchedulingAlgorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
