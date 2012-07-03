59071d4b76e9ca41d5a65ad49a993abcd277c0fe
llaguilo.m@gmail.com <llaguilo.m@gmail.com@72b0d667-33a1-be38-4b05-428c798d882c>
2012-07-03 17:15:38 +0000

    Añadidos explicación de todas las clases y métodos de la práctica. Solucionados algunos problemas.

---
+++ trunk/scheduling-algorithms/src/scheduling/algorithms/Result.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scheduling.algorithms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Lluis
+ */
+public class Result {
+    
+    private class ResultData{
+        float  totalLoad;
+        String servername;
+        float  meanResponseTime;
+        
+        public ResultData(float totalLoad, String servername, float meanResponseTime){
+            this.totalLoad        = totalLoad;
+            this.servername       = servername;
+            this.meanResponseTime = meanResponseTime;
+        }
+    }
+    
+    
+    
+    private ArrayList<ResultData> resultTable;
+    
+    public Result(){
+        resultTable = new ArrayList<>();
+    }
+    
+    public void setData(float total_load, String serverName, float meanResponseTime){
+        resultTable.add(new ResultData(total_load, serverName, meanResponseTime));
+    }
+    
+    public float getData(float total_load,String serverName){
+        Iterator<ResultData> i = resultTable.iterator();
+        ResultData r = new ResultData(-1, serverName, -1);
+        boolean found = false;
+        while (i.hasNext() && ! found){
+            r = i.next();
+            found = r.servername.equals(serverName)&& r.totalLoad == total_load;
+        }
+        
+        return found ? r.meanResponseTime : -1.0f;
+        
+    }
+    
+    public void saveResultsOdtFormat(String url_file_name){
+        throw new UnsupportedOperationException("SaveResultsOdtFormat not implemented yet.");
+    }
+    
+}
