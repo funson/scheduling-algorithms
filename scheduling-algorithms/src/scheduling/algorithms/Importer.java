78e8e04b81e046db1cde9f0685c9de1f4c9c71ae
miquel.amj <miquel.amj@gmail.com>
2012-07-04 10:48:04 +0000

    Importer sense acabar, falta asignar la informació a la estructura de tasques des programa. El fitxer de texte, obert amb Excel, Guardar como .txt.

--- scheduling-algorithms/src/scheduling/algorithms/Importer.java
+++ scheduling-algorithms/src/scheduling/algorithms/Importer.java
@@ -4,7 +4,13 @@
  */
 package scheduling.algorithms;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Clase que gestiona la entrada de datos a la aplicación.
@@ -21,6 +27,71 @@ public class Importer {
     public static void importTaskSets(String urlFile, ArrayList<TaskSet> taskSets, Server[] servers){
         taskSets   = new ArrayList<>();
         servers[0] = new BackgroundServer();
-        throw new UnsupportedOperationException("Import task sets not supported yet.");        
+        throw new UnsupportedOperationException("Import task sets not supported yet.");
+        
+        
+        // Informació de fitxer
+        int currentSets = -1;
+        // Utilització dels servidors
+        float u;
+        
+        int taskGroup; // el grup de tasques actual, on s'han d'insertar les tasques (index)
+        // Informació de la tasca actual
+        String nom; 
+        float t, c, ph;
+        
+        
+        String sLine;
+        BufferedReader bf = null;
+        try {
+            bf = new BufferedReader(new FileReader("C:/Users/Miguel/Desktop/Dades_generiques (1).txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Importer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            while ((sLine = bf.readLine())!=null) {
+                String aux[] = sLine.split("\t");
+                int i = 0;
+                while (i < aux.length){
+                    if (!aux[i].equals("") ){
+                        if (aux[i].contains("Up=")){
+                            //System.out.println(aux[i].substring(3, aux[i].length()-1));
+                            u = Integer.parseInt(aux[i].substring(3, aux[i].length()-1));
+                            System.out.println(u);
+                            currentSets++;
+                        }
+                        else if (aux[i].contains("Grup tasques")){
+                            //System.out.println(aux[i].charAt(aux[i].length()-1));
+                            taskGroup = Integer.parseInt(aux[i].substring(aux[i].length()-1));
+                            System.out.println(taskGroup);
+                        }
+                        else if (aux[i].contains("Px_")){
+                            nom = aux[i];
+                            i++; // Passam al seg item
+                            while (aux[i].equals("\t") || aux[i].equals("") || aux[i].equals(" ")) i++;
+                            t = Float.parseFloat(aux[i].replace(",", "."));
+                            i++; // Passam al seg item
+                            while (aux[i].equals("\t") || aux[i].equals("") || aux[i].equals(" ")) i++;
+                            c = Float.parseFloat(aux[i].replace(",", "."));
+                            i++; // Passam al seg item
+                            while (aux[i].equals("\t") || aux[i].equals("") || aux[i].equals(" ")) i++;
+                            ph = Float.parseFloat(aux[i].replace(",", "."));
+                            
+                            
+                            System.out.println("Nom: " + nom);
+                            System.out.println("Periode: " + t);
+                            System.out.println("Temps de computació: " + c);
+                            System.out.println("Fase: " + ph);
+                            
+                            
+                            //arraydetasques.insert(taskGroup).
+                        }
+                    }
+                    i++;
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Importer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
